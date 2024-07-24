e28995a7288f6378d52651dc976c52a3a00dbd98
Kush Kothari <kushkothari2001@gmail.com>
2024-07-24 15:20:40 +0530

    change to build

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   projectName: "notes-web", // Usually your repo name.
   deploymentBranch: "gh-pages",
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
